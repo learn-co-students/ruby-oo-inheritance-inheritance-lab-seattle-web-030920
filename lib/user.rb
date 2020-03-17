796daf962d655b41ab6c38a0e09b8ceee6c8a298
wakennedy <alexkenned@gmail.com>
2020-03-17 13:07:00 -0700

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+class User
+    attr_accessor :first_name, :last_name
+    # @@all = []
+    # def initialize(first_name, last_name)
+    #     @first_name = first_name
+    #     @last_name = last_name
+    #     @@all << self
+    # end
+    # def first_name
+    #     @first_name
+    # end
+    # def last_name
+    #     @last_name
+    # end
+
+
+end
