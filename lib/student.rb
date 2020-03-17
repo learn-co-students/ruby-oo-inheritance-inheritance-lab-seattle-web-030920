fcb0807d640f29c0d4f46a26c135772c42f9d25c
djknudson <djknudson@icloud.com>
2020-03-17 13:47:35 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(data)
+        @knowledge << data
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
